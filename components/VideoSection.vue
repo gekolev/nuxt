9a62feb890cc0aeb19545e4112985266f46ea1e0
George Kolev <gogo1520180@gmail.com>
2022-09-09 11:49:28 +0300

    init

---
+++ components/VideoSection.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Video Section
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <Tutorial />
+  <VideoSection />
+</div>
+</template>
+
+<script>
+import VideoSection from '../components/VideoSection.vue';
+export default {
+    name: "IndexPage",
+    components: { VideoSection }
+}
+</script>
